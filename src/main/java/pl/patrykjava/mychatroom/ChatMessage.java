f49cbfaffa26563c171fa9b242f9b08bae5048b9
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-11-20 18:23:44 +0100

    Implement Chat config

---
+++ src/main/java/pl/patrykjava/mychatroom/ChatMessage.java
@@ -0,0 +1,15 @@
+package pl.patrykjava.mychatroom;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+}
---
+++ src/main/java/pl/patrykjava/mychatroom/MessageType.java
@@ -0,0 +1,6 @@
+package pl.patrykjava.mychatroom;
+
+public enum MessageType {
+
+    CHAT, JOIN, DISCONNECT
+}
