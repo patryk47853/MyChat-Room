54fb07b5c84b8c6658cea26ac97d2b66b2c54312
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-11-20 17:37:20 +0100

    Initial commit

--- src/main/java/pl/patrykjava/mychatroom/MychatRoomApplication.java
+++ src/main/java/pl/patrykjava/mychatroom/MychatRoomApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MychatRoomApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MychatRoomApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MychatRoomApplication.class, args);
+    }
 
 }
