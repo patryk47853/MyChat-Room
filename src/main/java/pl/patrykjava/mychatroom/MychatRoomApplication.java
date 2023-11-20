8967f089b77eecadc99e272730c747fad2a13324
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-11-20 17:35:56 +0100

    Initial commit

---
+++ src/main/java/pl/patrykjava/mychatroom/MychatRoomApplication.java
@@ -0,0 +1,13 @@
+package pl.patrykjava.mychatroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MychatRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MychatRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/patrykjava/mychatroom/MychatRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.patrykjava.mychatroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MychatRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
