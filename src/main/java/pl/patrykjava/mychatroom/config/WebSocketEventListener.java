c5f133b6e22a3a585f25170d937505e623a1903c
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-11-21 19:04:36 +0100

    Add handleWebSocketConnectListener method and implement ApplicationStats methods

--- src/main/java/pl/patrykjava/mychatroom/config/WebSocketEventListener.java
+++ src/main/java/pl/patrykjava/mychatroom/config/WebSocketEventListener.java
@@ -6,9 +6,13 @@ import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
-import pl.patrykjava.mychatroom.ChatMessage;
-import pl.patrykjava.mychatroom.MessageType;
+import pl.patrykjava.mychatroom.controller.ApplicationStats;
+import pl.patrykjava.mychatroom.model.ChatMessage;
+import pl.patrykjava.mychatroom.utils.MessageType;
+
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
@@ -18,12 +22,26 @@ public class WebSocketEventListener {
     private final SimpMessageSendingOperations messageTemplate;
 
     @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+
+        // Increment the new user count.
+        ApplicationStats.incrementUserCount();
+
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) Objects.requireNonNull(headerAccessor.getSessionAttributes()).get("username");
+
+        log.info("User connected {}", username);
+    }
+    @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        String username = (String) Objects.requireNonNull(headerAccessor.getSessionAttributes()).get("username");
 
         if (username != null) {
+            ApplicationStats.decrementUserCount();
+
             log.info("User disconnected {}", username);
+
             var chatMessage = ChatMessage.builder()
                     .type(MessageType.DISCONNECT)
                     .sender(username)
