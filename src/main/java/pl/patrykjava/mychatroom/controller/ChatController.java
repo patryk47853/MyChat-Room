6a39f80eb03f085136dec38150788d833a557b3c
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-11-22 14:17:20 +0100

    Add SimpMessagingTemplate and method to update current users count

--- src/main/java/pl/patrykjava/mychatroom/controller/ChatController.java
+++ src/main/java/pl/patrykjava/mychatroom/controller/ChatController.java
@@ -4,15 +4,27 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
-import pl.patrykjava.mychatroom.ChatMessage;
+import pl.patrykjava.mychatroom.model.ChatMessage;
+import pl.patrykjava.mychatroom.utils.ApplicationUtils;
+
+import java.util.Objects;
 
 @Controller
 public class ChatController {
 
+    private final SimpMessagingTemplate template;
+
+    public ChatController(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage (@Payload ChatMessage chatMessage) {
+        chatMessage.setTime(ApplicationUtils.getTime());
         return chatMessage;
     }
 
@@ -20,7 +32,13 @@ public class ChatController {
     @SendTo("/topic/public")
     public ChatMessage addUser (@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         // Add username in WebSocket session
-        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("username", chatMessage.getSender());
         return chatMessage;
+    }
+
+    @Scheduled(fixedDelay = 1000)
+    public void sendAdhocMessage() {
+        // Send the updated user count to the chat.
+        template.convertAndSend("/topic/stats", ApplicationStats.getUserCount());
     }
 }
